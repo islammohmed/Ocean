6196249cb0131a98de5bb9e0f09a25d13e5036bc
islammohmed <islammohamed3235@gmail.com>
2024-10-05 03:41:31 +0300

    ocean

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { FC, ReactNode } from "react";
+
+interface ButtonProps {
+    variant?: "primary" | "secondary" | "outline";
+    isFullSize?: boolean;
+    children: ReactNode;
+}
+
+const Button: FC<ButtonProps> = ({
+    variant = "primary",
+
+    children,
+}) => {
+    return (
+        <button
+            className={`btn ${variant === "primary"
+                    ? "btn-primary"
+                    : variant === "secondary"
+                        ? "btn-secondary"
+                        : "btn-outline"
+                }`}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
---
+++ src/constants/clients.tsx
@@ -0,0 +1,21 @@
+import { Client } from "@/types/interfaces";
+
+
+export const clients: Client[] = [
+    {
+        imageUrl: '/companies/company-1.svg',
+        alt: 'company1'
+    },
+    {
+        imageUrl: '/companies/company-2.svg',
+        alt: 'company1'
+    },
+    {
+        imageUrl: '/companies/company-3.svg',
+        alt: 'company1'
+    },
+    {
+        imageUrl: '/companies/company-4.svg',
+        alt: 'company1'
+    },
+]
